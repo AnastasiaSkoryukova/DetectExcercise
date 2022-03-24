c45f25012a3ca66779caf1576f0ffbc9349e599c
AnastasiaSkoryukova <65908650+AnastasiaSkoryukova@users.noreply.github.com>
2022-03-24 14:51:08 +0100

    Add project with the trained model

--- GuessMyExercise/Action Classifier/ExerciseClassifier+Label.swift
+++ GuessMyExercise/Action Classifier/ExerciseClassifier+Label.swift
@@ -8,20 +8,20 @@ Defines the app's knowledge of the model's class labels.
 extension ExerciseClassifier {
     /// Represents the app's knowledge of the Exercise Classifier model's labels.
     enum Label: String, CaseIterable {
-        case lunges = "Lunges"
-        case burpees = "Burpees"
-        case jumpingJacks = "Jumping Jacks"
-
         /// A negative class that represents irrelevant actions.
-        case otherAction = "Other Action"
-
+        case random = "random"
+        
+        case reach = "reach"
+        case squats = "squats"
+        
+        
         /// Creates a label from a string.
         /// - Parameter label: The name of an action class.
         init(_ string: String) {
             guard let label = Label(rawValue: string) else {
                 let typeName = String(reflecting: Label.self)
                 fatalError("Add the `\(string)` label to the `\(typeName)` type.")
-            }
+            }   
 
             self = label
         }
